3a8affa1986917e58be8f71cfb679747219ee56c
Blake <gn4514@wayne.edu>
2021-05-19 08:23:20 -0400

    work done

--- collector.py
+++ collector.py
@@ -1,7 +1,12 @@
 import discord
+import sys
+import os
+
+from dotenv import load_dotenv
 from discord.ext import commands
 
-TOKEN="ODQzODg1NTM1ODAxNjM4OTEz.YKKXpg.bHXhIA4cIHDow2jhBucb3QECeac"
+load_dotenv()
+TOKEN=os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='$')
 
@@ -14,10 +19,19 @@ async def add_mod(ctx, *args):
     file.close
     
 @bot.command(name='list')
-async def print_list(ctx):
-    await ctx.send('Listing mods')
-    file = open('mods.txt')
-    await ctx.send(file.read())
+async def print_list(ctx, *args):
+    if len(args) == 0:
+        await ctx.send('Listing mods')
+        file = open('mods.txt')
+        await ctx.send(file.read())
+    if args == 'clear':
+        await ctx.send('Mods list cleared')
+        os.remove('mods.txt')
+
+@bot.command(name='stop')
+async def stop_bot(ctx):
+    await ctx.bot.logout()
+    await login(TOKEN, bot=True)
 
 
 bot.run(TOKEN)
